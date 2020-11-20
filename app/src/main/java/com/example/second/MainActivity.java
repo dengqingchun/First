d4539f64a7d21459665f94b344fbdb27ba3c89db
dengqingchun <dengqingchun1999@163.com>
2020-11-20 10:11:34 +0800

    Demo

--- app/src/main/java/com/example/second/MainActivity.java
+++ app/src/main/java/com/example/second/MainActivity.java
@@ -1,14 +1,12 @@
 package com.example.second;
 
-import androidx.appcompat.app.AppCompatActivity;
+import android.content.Intent;
+import android.content.SharedPreferences;
+import android.preference.PreferenceManager;
 
 import android.os.Bundle;
-import android.view.View;
-import android.widget.Button;
 
-import com.example.second.litepal.Album;
-
-import org.litepal.tablemanager.Connector;
+import androidx.appcompat.app.AppCompatActivity;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -16,32 +14,12 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        //创建数据库表
-        Button createDatabase = (Button) findViewById(R.id.create_database);
-        createDatabase.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                Connector.getDatabase();
-            }
-        });
-        //添加数据到表
-        Button addData = (Button) findViewById(R.id.add_data);
-        addData.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                Album album = new Album();
-                album.setName("Second");
-                album.setPrice(1);
-                album.save();
-            }
-        });
-        Button updateData = (Button) findViewById(R.id.update_data);
-        updateData.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-
-            }
-        });
-
+        SharedPreferences prefs = PreferenceManager.getDefaultSharedPreferences(this);
+        if (prefs.getString("weather", null) != null) {
+            Intent intent = new Intent(this, WeatherActivity.class);
+            startActivity(intent);
+            finish();
+        }
     }
+
 }
