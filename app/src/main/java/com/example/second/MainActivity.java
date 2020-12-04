8b6259f1fc39b57321a13e1fd9f7b4530be97be3
dengqingchun <dengqingchun1999@163.com>
2020-12-04 08:51:02 +0800

    Demo

--- app/src/main/java/com/example/second/MainActivity.java
+++ app/src/main/java/com/example/second/MainActivity.java
@@ -10,7 +10,7 @@ import androidx.appcompat.app.AppCompatActivity;
 
 public class MainActivity extends AppCompatActivity {
 
-
+ 
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
